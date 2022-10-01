71fc5f963da4f57497d1d7a439a3b171f556364f
daiki.s <daiki.s@itnav.co.jp>
2022-10-01 15:06:27 +0900

    add: vue-router pinia

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
+import {createPinia } from "pinia"
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import router from "./router"
 import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
 
 createApp(App)
+  .use(createPinia())
   .use(vuetify)
+  .use(router)
   .mount('#app')
